2db525cc17ee01e9769c783fabfc2141b77e3f13
Mohit Dhale <43096331+mohitdhale@users.noreply.github.com>
2021-03-26 14:55:07 +0530

    Update testing.m

--- testing.m
+++ testing.m
@@ -1,6 +1,6 @@
 
 %function[output]=signature(img)
-input_dir = 'C:/Users/Admin/Desktop/CitrusImage3/Data Testing';
+input_dir = '/Data Testing';
 
 
 filenames = dir(fullfile(input_dir, '*.jpg'));
@@ -15,7 +15,7 @@ for n = 1:length(filenames)
     filename = fullfile(input_dir, filenames(n).name);
     img = imread(filename);
     img = imresize(img,[256,256]);
- figure, imshow(img);title('Selected Image');
+    figure, imshow(img);title('Selected Image');
 
     r=img(:,:,1);
     
@@ -24,23 +24,23 @@ for n = 1:length(filenames)
     g=img(:,:,3);
    
     m1=mean(mean(r));
-   [r,c]=size(img);
-   mr=zeros(r,c);
-   mb=zeros(r,c);
-   mg=zeros(r,c);
+    [r,c]=size(img);
+    mr=zeros(r,c);
+    mb=zeros(r,c);
+    mg=zeros(r,c);
     m2=mean(mean(b));
     
     m3=mean(mean(g));
-   mri=m1+m2+m3/m1;
-   mbi=m1+m2+m3/m2;
-   mgi=m1+m2+m3/m3;
+    mri=m1+m2+m3/m1;
+    mbi=m1+m2+m3/m2;
+    mgi=m1+m2+m3/m3;
    
     mri_test1(k,256:768)=mri;
     mbi_test1(k,256:768)=mbi;
     mgi_test1(k,256:758)=mgi;
-label_test1(k,1:256)=i;
-k=k+1;
-   end
+    label_test1(k,1:256)=i;
+    k=k+1;
+ end
   
  
  mri_test= mri_test1;
@@ -48,7 +48,7 @@ k=k+1;
  mgi_test=mgi_test1;
  label_test=label_test1
 
-save('C:/Users/Admin/Desktop/citrusimage3/Data Testing/testset.mat','mri_test','mbi_test','mgi_test','label_test');
+save('Data Testing/testset.mat','mri_test','mbi_test','mgi_test','label_test');
 
     
     
